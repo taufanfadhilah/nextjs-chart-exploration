2bc16a2802bb9dc56900ccee886f0ace796ce1a0
taufanfadhilah <taufanfadhilahiskandar@gmail.com>
2021-03-24 10:15:41 +0700

    setup bootstrap v5

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,18 @@
-import '../styles/globals.css'
+import Head from "next/head";
+// add bootstrap css
+import "bootstrap/dist/css/bootstrap.css";
+
+import "../styles/globals.css";
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <>
+      <Head>
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+      </Head>
+      <Component {...pageProps} />
+    </>
+  );
 }
 
-export default MyApp
+export default MyApp;
