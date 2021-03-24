0d0ec4f052579dc24b3aac3421a4d253d3915b68
taufanfadhilah <taufanfadhilahiskandar@gmail.com>
2021-03-24 11:40:45 +0700

    trading view page

---
+++ pages/tradingView/index.js
@@ -0,0 +1,13 @@
+const Index = () => {
+  return (
+    <div className="container mt-5">
+      <div className="row">
+        <div className="col-md-12 text-center">
+          <h1>Trading View Chart</h1>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Index;
